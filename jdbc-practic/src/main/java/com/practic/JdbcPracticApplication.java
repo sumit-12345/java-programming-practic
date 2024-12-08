cc759f332b0757b5e08190c25f83a869ea6021f5
sumit-12345 <sumitKu.Joshi@gmail.com>
2024-12-08 20:17:47 +0530

    test

---
+++ jdbc-practic/src/main/java/com/practic/JdbcPracticApplication.java
@@ -0,0 +1,13 @@
+package com.practic;
+
+/**
+ * Hello world!
+ *
+ */
+public class JdbcPracticApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
