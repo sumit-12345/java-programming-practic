296a90eaea463aad36227b6961e83fb5e312739b
sumit-12345 <sumitKu.Joshi@gmail.com>
2025-01-06 22:37:41 +0530

    test

--- collection-framework/src/main/java/com/practic/App.java
+++ collection-framework/src/main/java/com/practic/App.java
@@ -1,41 +1,26 @@
 package com.practic;
 
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Person person = new Person(10, "b", "1234", "USA");
-        Person person1 = new Person(1, "y", "1282", "UK");
-        Person person2 = new Person(101, "y", "12456", "SA");
-
-        List<Person> personList = new ArrayList<>();
-        personList.add(person);
-        personList.add(person1);
-        personList.add(person2);
-        System.out.println(personList);
-        PersonComparator personComparator = new PersonComparator();
-        personList.sort(personComparator);
-        System.out.println(personList);
-    }
-
-    public static List<Student> generateStudents() {
-        List<Student> students = new CopyOnWriteArrayList<>();
-
-        // Add 30 students with some sample values
-        for (int i = 1; i <= 30; i++) {
-            // Example: Creating students with names like "Student1", "Student2", etc.
-            String name = "Student" + i;
-            int rollNo = i;
-            String grade = (i % 2 == 0) ? "A" : "B"; // Alternating grades for example
-
-            students.add(new Student(name, rollNo, grade));
+        String document = "ABC XYZ ABC XYZ MNO ABC PQR MNO ABC";
+        String[] words = document.split(" ");
+        Map<String,Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                Integer i = map.get(word);
+                map.put(word,i.intValue()+1);
+            }else {
+                map.put(word,1);
+            }
         }
 
-        return students;
+        for (String key : map.keySet()) {
+            System.out.println(key + " -> "+ map.get(key));
+        }
     }
 }
